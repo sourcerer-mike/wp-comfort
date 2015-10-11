b221ecfa4bb1566b9fda7ad79c00b3aea810eb20
Mike Pretzlaw <pretzlaw@gmail.com>
2015-10-11 18:58:04 +0200

    Sami: Append root files

--- tests/sami.php
+++ tests/sami.php
@@ -6,15 +6,28 @@ require_once ABSPATH . '/wp-admin/includes/plugin.php';
 
 $data = get_plugin_data( COMFORT_FILE );
 
-$versions = \Sami\Version\GitVersionCollection::create(dirname(__DIR__))
-	->addFromTags('v1.*.*')
-	->add('master');
+$versions = \Sami\Version\GitVersionCollection::create( dirname( __DIR__ ) )
+                                              ->addFromTags( 'v1.*.*' )
+                                              ->add( 'master' )
+;
+
+$iterator = new \Symfony\Component\Finder\Finder();
+$iterator->files()
+         ->in( COMFORT_DIR . '/includes' )
+         ->name( '*.php' )
+         ->append(
+	         \Symfony\Component\Finder\Finder::create()
+	                                         ->in( COMFORT_DIR )
+	                                         ->name( '*.php' )
+	                                         ->depth( 0 )
+         )
+;
 
 $config = new Sami\Sami(
-	__DIR__ . '/../includes',
+	$iterator,
 	[
-		'versions' => $versions,
-		'title' => 'Comfort',
+		'versions'  => $versions,
+		'title'     => 'Comfort',
 		'build_dir' => COMFORT_DIR . '/var/sami/%version%',
 		'cache_dir' => COMFORT_DIR . '/var/sami/cache/%version%',
 	]
