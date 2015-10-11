2c03b102640d89f18cf560448ae201b84fa96a89
Mike Pretzlaw <pretzlaw@gmail.com>
2015-10-11 18:19:31 +0200

    KISS

--- comfort.php
+++ comfort.php
@@ -10,17 +10,9 @@ Text Domain: comfort
 Domain Path: /languages
 */
 
-if ( ! defined( 'COMFORT_DIR' ) ) {
-	define( 'COMFORT_DIR', __DIR__ );
-}
-
-if ( ! defined( 'COMFORT_FILE' ) ) {
-	define( 'COMFORT_FILE', __FILE__ );
-}
-
-if ( ! defined( 'COMFORT_TEXTDOMAIN' ) ) {
-	define( 'COMFORT_TEXTDOMAIN', basename( __DIR__ ) );
-}
+@define( 'COMFORT_DIR', __DIR__ );
+@define( 'COMFORT_FILE', __FILE__ );
+@define( 'COMFORT_TEXTDOMAIN', basename( __DIR__ ) );
 
 // Tear-up plugin and load all files in the include-directory.
 require_once 'bootstrap.php';
