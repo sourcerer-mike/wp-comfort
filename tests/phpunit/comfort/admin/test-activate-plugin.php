8f7bb034ccf6637209edfb25e2a9072a0f760f51
Mike Pretzlaw <pretzlaw@gmail.com>
2015-10-11 21:20:23 +0200

    PHPUnit: Activate plugin ignores files out of version range

--- tests/phpunit/comfort/admin/test-activate-plugin.php
+++ tests/phpunit/comfort/admin/test-activate-plugin.php
@@ -7,13 +7,18 @@ use Comfort\TestCase;
 class Activate_Plugin_Test extends TestCase {
 	private static $backup_version = false;
 
+	public function filterPluginData( $data ) {
+		$data['Version'] = $this->getDummyVersion();
+
+		return $data;
+	}
+
 	/**
 	 * @return string
 	 */
-	public function getDummyScriptPathname() {
-		return $this->getPluginDirectory()
-		       . '/includes/setup/'
-		       . $this->getDummyVersion() . '-foo.php';
+	public
+	function getDummyVersion() {
+		return '99.99.99';
 	}
 
 	protected function setUp() {
@@ -22,22 +27,11 @@ class Activate_Plugin_Test extends TestCase {
 
 	}
 
-	protected function tearDown() {
-		$this->tearDownDbVersion();
-		$this->tearDownDummyScript();
-	}
+	protected function setUpDbVersion() {
+		static::$backup_version = get_option( $this->getDbName() );
 
-	public function filterPluginData( $data ) {
-		$data['Version'] = $this->getDummyVersion();
-
-		return $data;
-	}
-
-
-	public function test_setup_scripts_are_rolled_out() {
-		comfort_activate();
-
-		$this->assertEquals( $this->getDummyVersion(), get_option( $this->getDbName() ) );
+		add_filter( 'plugin_data', [ $this, 'filterPluginData' ] );
+		update_option( $this->getDbName(), '99.99.98' );
 	}
 
 	/**
@@ -53,16 +47,18 @@ class Activate_Plugin_Test extends TestCase {
 		touch( $setup_dummy );
 	}
 
-	private function tearDownDummyScript() {
-		unlink( $this->getDummyScriptPathname() );
-	}
-
 	/**
 	 * @return string
 	 */
-	public
-	function getDummyVersion() {
-		return '99.99.99';
+	public function getDummyScriptPathname() {
+		return $this->getPluginDirectory()
+		       . '/includes/setup/'
+		       . $this->getDummyVersion() . '-foo.php';
+	}
+
+	protected function tearDown() {
+		$this->tearDownDbVersion();
+		$this->tearDownDummyScript();
 	}
 
 	protected function tearDownDbVersion() {
@@ -73,11 +69,38 @@ class Activate_Plugin_Test extends TestCase {
 		}
 	}
 
-	protected function setUpDbVersion() {
-		static::$backup_version = get_option( $this->getDbName() );
+	private function tearDownDummyScript() {
+		unlink( $this->getDummyScriptPathname() );
+	}
 
-		add_filter( 'plugin_data', [ $this, 'filterPluginData' ] );
-		update_option( $this->getDbName(), '99.99.98' );
+	public function testItSkipsOldScripts() {
+		$oldScript = dirname( $this->getDummyScriptPathname() ) . '/0.0.0-test.php';
+
+		file_put_contents( $oldScript, '<?php throw new \\Exception();' );
+
+		comfort_activate();
+
+		$this->assertNotContains( $oldScript, get_included_files() );
+
+		unlink( $oldScript );
+	}
+
+	public function testItIgnoresScriptsNewerThanThePluginVersion() {
+		$newScript = dirname( $this->getDummyScriptPathname() ) . '/999.0.0-test.php';
+
+		file_put_contents( $newScript, '<?php throw new \\Exception();' );
+
+		comfort_activate();
+
+		$this->assertNotContains( $newScript, get_included_files() );
+
+		unlink( $newScript );
+	}
+
+	public function test_setup_scripts_are_rolled_out() {
+		comfort_activate();
+
+		$this->assertEquals( $this->getDummyVersion(), get_option( $this->getDbName() ) );
 	}
 }
 
