084c839477370731d51a1f820fef6357f77cd57d
Mike Pretzlaw <pretzlaw@gmail.com>
2015-10-28 05:57:05 +0100

    Assert correct plugin version in DB after all tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,3 +8,12 @@ require_once ABSPATH . '/wp-admin/includes/plugin.php';
 $plugin_file = plugin_basename( dirname( __DIR__ ) . '/comfort.php' );
 deactivate_plugins( [ $plugin_file ] );
 activate_plugin( $plugin_file );
+
+// assert correct version after tests
+define( 'COMFORT_VERSION', get_site_option( 'comfort_version', '0.0.0' ) );
+
+register_shutdown_function(
+	function () {
+		update_option( 'comfort_version', COMFORT_VERSION );
+	}
+);
